6d1859657bdef10ae2bd7690f90a9452192fe7a2
Linas Ziedas <linas@digitalmojo.com>
2013-07-03 18:48:47 +0300

    merge & sort

--- merge_sort.php
+++ merge_sort.php
@@ -1,14 +1,69 @@
 <?php
 
-$data = array(7, 2, 4, 3, 8, 1, 6, 5);
+$data = array(5, 2, 3, 1, 4, 6);
+print_r($data);
 $length = count($data);
+// merge(array_slice($data, 0, $length / 2), array_slice($data, $length / 2));
 
-$left = array_slice($data, 0, $length / 2); 
-$right = array_slice($data, $length / 2); 
+$l = divide(array_slice($data, 0, $length / 2));
+$r = divide(array_slice($data, $length / 2));
 
-print_r($left);
-print_r($right);
+print_r(mmerge($l, $r));
 
-for ($i=0; $i<count($left); $i++) {
-	print_r($left[$i]);
+function divide($item) {
+    $length = count($item);
+    if ($length == 2) {
+        return conquer($item[0], $item[1]);
+    }
+
+    $left = array_slice($item, 0, $length / 2);
+    $right = array_slice($item, $length / 2);
+
+    return merge($left, $right);
 }
+
+function conquer($left, $right) {
+    return
+        $left < $right
+            ? array($left, $right)
+            : array($right, $left);
+}
+
+function mmerge($left, $right) {
+    $item = array();
+    $i=0; $j=0;
+    while (count($item) < (count($left) + count($right))) {
+        if ($i >= count($left)) {
+            $item[] = $right[$j++];
+            continue;
+        } elseif ($j>=count($right)) {
+            $item[] = $left[$i++];
+            continue;
+        }
+        // elseif (count($left) == $i && count($right) == $j) {
+        //     break;
+        // } else {
+            if ($left[$i] < $right[$j]) {
+                $item[] = $left[$i++];
+            } else {
+                $item[] = $right[$j++];
+            }
+        // }
+    }
+
+    return $item;
+
+}
+function merge($left, $right) {
+    if (count($left) > 2) {
+        divide($left);
+    }
+    if (count($right) > 2) {
+        divide($right);
+    }
+
+    $left = conquer($left[0], $left[1]);
+    $right = conquer($right[0],$right[1]);
+
+    return mmerge($left, $right);
+    }

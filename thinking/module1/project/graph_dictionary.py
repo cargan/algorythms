c03b7c1567c155936fdae34082850766e90492ad
Linas Žiedas <ziedas@gmail.com>
2014-08-29 15:41:51 +0300

    ini

---
+++ thinking/module1/project/graph_dictionary.py
@@ -0,0 +1,8 @@
+#https://class.coursera.org/algorithmicthink-001/wiki/graph_degree
+
+EX_GRAPH0 = {0: {1, 2}, 1:{}, 2:{}}
+EX_GRAPH1 = {0: {1, 4, 5}, 1: {2, 6}, 2: {3}, 3: {0}, 4: {1}, 5: {2}, 6: {}}
+EX_GRAPH2 = {}
+
+print EX_GRAPH0
+
